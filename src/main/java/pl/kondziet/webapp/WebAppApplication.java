7be5fbada664f3ffe5ecbb7a2496af4eecd776fd
kondziet <konrad.dabrowski54@gmail.com>
2023-03-08 09:27:34 +0100

    Add initial files

---
+++ src/main/java/pl/kondziet/webapp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package pl.kondziet.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppApplication.class, args);
+    }
+
+}
